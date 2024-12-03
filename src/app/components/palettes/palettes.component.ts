9dbce830ce4c8227dad3adbba5b264e13c34bc68
EltonJCS <eltjcs@gmail.com>
2024-12-03 01:21:47 -0300

    Improve color palette display

--- src/app/components/palettes/palettes.component.ts
+++ src/app/components/palettes/palettes.component.ts
@@ -9,8 +9,21 @@ import { GetColorPalettesService } from '../../services/get-color-palettes.servi
 })
 export class PalettesComponent {
   private getPaletteModels = inject(GetColorPalettesService);
+  colors: string[] = [];
 
   getModels(){
 this.getPaletteModels.getPaletteModels();
+  }
+
+  getLocalStorageColors(){
+    for (let i = 0; i < 5; i++) {
+      const color = localStorage.getItem(`color${i}`);
+      this.colors.push(color || '#ffffff');
+    }
+  }
+
+  showColors(){
+    this.getModels();
+    this.getLocalStorageColors();
   }
 }
--- src/app/services/get-color-palettes.service.ts
+++ src/app/services/get-color-palettes.service.ts
@@ -12,8 +12,14 @@ private UrlParameters: string = 'scheme?hex=ff0000&mode=monochrome';
   constructor(private http: HttpClient) { }
 
   async getPaletteModels(){
-    this.http.get(this.apiUrl + this.UrlParameters).subscribe(response => {
-      console.log(response);
+    this.http.get<{ [key: string]: any[] }>(this.apiUrl + this.UrlParameters).subscribe(response => {
+      const data = response;
+      console.log(data["colors"]);
+      localStorage.setItem("color0", data["colors"]["0"]["hex"]["value"]);
+      localStorage.setItem("color1", data["colors"]["1"]["hex"]["value"]);
+      localStorage.setItem("color2", data["colors"]["2"]["hex"]["value"]);
+      localStorage.setItem("color3", data["colors"]["3"]["hex"]["value"]);
+      localStorage.setItem("color4", data["colors"]["4"]["hex"]["value"]);
     });
   }
 }
