ed92ab5a659241f96511b31cab236036fb0224df
EltonJCS <eltjcs@gmail.com>
2024-12-07 18:13:14 -0300

    Add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PalettesComponent } from './components/palettes/palettes.component';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PalettesComponent],
+  imports: [RouterOutlet, PalettesComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
